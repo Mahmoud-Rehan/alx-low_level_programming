2613c8fa437b3cb8cd404284f8fa3601ca418a9d
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-07-13 13:30:47 -0700

    C language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
  *main - print the line betwwen the quotes
- *return - (0)  always
- * /
-int main (void) 
+ *return: 0 always
+ */
+int main (void)
 {
 	puts ("\"Programming is like building a multilingual puzzle\n");
 	return (0);
