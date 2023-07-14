912255be3e1c7cd4e5bad2a2f306ce69e7b50611
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-07-14 15:42:18 -0700

    C language

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - print all alphabets in lowecase
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	char a = 'a';
 
-	while (a != 'z')
+	while (a != ('z' + 1))
 	{
 		putchar(a);
 		a++;
