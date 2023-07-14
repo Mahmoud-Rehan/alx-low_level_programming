ed64f4d33d344a903e05c826314503a49c5b3bd4
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-07-14 16:28:35 -0700

    C language

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphabets accept for 'q' and 'e'.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c == 'q' || c == 'e')
+		{
+			continue;
+		}
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
