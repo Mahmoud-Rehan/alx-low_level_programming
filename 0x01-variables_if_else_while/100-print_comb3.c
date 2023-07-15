086238aed9ba2785b8591e8301ca2e08e443b500
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-07-15 08:22:31 -0700

    C language

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible different combinations of two digits.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+	int i;
+
+	for (i = 48; i < 57; i++)
+	{
+		for (n = 49; n <= 57; n++)
+		{
+			putchar(i);
+			putchar(n);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
