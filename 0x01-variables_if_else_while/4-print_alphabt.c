c997b78f1b7fc604a6ff1bdedd1fd6d7ba2e2ce3
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-07-14 17:00:55 -0700

    C language

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,14 +10,13 @@
 
 int main(void)
 {
-	int i = 97;
+	int i;
 
-	while (i <= 122)
+	for (i = 97; i <= 122; i++)
 	{
 		if (i != 101 && i != 113)
 		{
 			putchar(i);
-			i++;
 		}
 	}
 	putchar('\n');
