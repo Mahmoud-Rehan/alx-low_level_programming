e781bf325f05158e20dadcd0d96134c149b82158
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-07-15 05:33:13 -0700

    C language

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,11 +10,14 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i < 58; i++)
+	for (i = 48; i <= 57; i++)
 	{
 		putchar(i);
-		putchar(44);
-		putchar(32);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}	
 	}
 	return (0);
 }
