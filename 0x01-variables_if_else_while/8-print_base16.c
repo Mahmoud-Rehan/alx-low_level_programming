440187a14419371b27e4f16279e0f7eee578f08e
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-07-15 05:15:40 -0700

    C language

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print all numbers of base 16 in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while(n < 16)
+	{
+		printf("%x", n);
+	}
+	putchar('\n');
+	return (0);
+}
