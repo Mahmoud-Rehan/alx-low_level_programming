868ffd6b52016e6b10067be7655dc870a31feb2c
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-07-18 11:25:51 -0700

    C language

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _abs - Return the absolute value.
+ *
+ * @n: The checked integer.
+ *
+ * Return: 0 or 1 or +n
+ */
+
+int _abs(int n)
+{
+	if (n == 0)
+	{
+		return (0);
+	}
+	else if (n < 0)
+	{
+		return (n + n*2);
+	}
+	else
+	{
+		return (n);
+	}
+}
