4234f5f1d8d7c7a8a0446256879fc87b9e6ed813
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-07-18 06:31:11 -0700

    C language

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,8 +2,6 @@
 
 /**
  * main - Print the alphabet function.
- *
- * Return: Always 0
  */
 
 void print_alphabet(void)
@@ -15,5 +13,4 @@ void print_alphabet(void)
 		_putchar('c');
 	}
 	_putchar('\n');
-	return (0);
 }
