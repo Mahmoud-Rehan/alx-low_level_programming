99f5f064e7a99e7ea97eac8076b11433c8c308f4
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-07-18 06:46:18 -0700

    C language

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_alphabet_10x - Prints 10 times the alphabet in lowercase.
+ * print_alphabet_x10 - Prints 10 times the alphabet in lowercase.
  */
 
-void print_alphabet_10x(void)
+void print_alphabet_x10(void)
 {
 	char c;
 	int i;
