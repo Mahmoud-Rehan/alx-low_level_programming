138e1817e5ba735a495d2c6cb571211d14873c36
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-07-18 06:29:51 -0700

    C language

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Print the alphabet function.
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	while (c <= 'z')
+	{
+		_putchar('c');
+	}
+	_putchar('\n');
+	return (0);
+}
