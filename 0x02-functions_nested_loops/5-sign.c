f858f798e93b87e151a80199f97c025c3ab4a37f
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-07-18 10:58:47 -0700

    C language

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints the sign of a number.
+ *
+ * #n: The checked integer
+ *
+ * Return: 0 or 1 or -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('1');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-1');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
