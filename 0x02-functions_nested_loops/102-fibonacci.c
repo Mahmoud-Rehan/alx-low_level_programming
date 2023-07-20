f1ab2ebc1d9d427d8404623e0ee1e4f3955eb71a
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-07-20 10:58:51 -0700

    C language

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int fibonacci(int n);
+
 int main(void)
 {
 	int n = 0;
@@ -8,7 +10,7 @@ int main(void)
 	printf("%d, ", sum);
 }
 
-void fibonacci(int n)
+int fibonacci(int n)
 {
 	int i = 0;
 
