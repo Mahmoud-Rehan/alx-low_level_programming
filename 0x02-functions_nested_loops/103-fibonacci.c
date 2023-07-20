e4ba114e3ff55109ad52703e76ec237e89c8d24b
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-07-20 14:40:51 -0700

    C language

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the sum of the even-valued terms
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	long n, m, r, sum;
+
+	m = 1;
+	r = n = sum = 0;
+	while (r < 4000000)
+	{
+		r = m + n;
+		if (r % 2 == 0)
+		{
+			sum = sum + r;
+		}
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
