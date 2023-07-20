5eb3f7f2fec4fe0f9ec9d6403bf1974930101b36
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-07-20 06:02:57 -0700

    C language

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+void print_times_table(int n)
+{
+	int i, j;
+
+	if (n < 0 || n > 15)
+	{
+		for (i = 0; i < n + 1; i++)
+		{
+			for (j = 0; j < n +1; j++)
+			{
+				if (j == 0)
+				{
+					_putchar('0' + (j * i));
+				}
+				else if (j * i < 10 && j * i != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((j * i) + '0');
+				}
+				else if (j * i < 100 && j * i >= 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(((j * i) / 10) + '0');
+					_putchar(((j * i) % 10) + '0');
+				}
+				else if (j * i >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(((j * i) / 100) + '0');
+					_putchar((((j * i) / 10) % 10) + '0');
+					_putchar(((j * i) % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
