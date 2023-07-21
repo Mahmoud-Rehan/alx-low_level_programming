249034a1c09ab7c1112463bce53f6994943a769b
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-07-21 14:44:57 -0700

    C language

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+
+		if (i != 100)
+		{
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+}
