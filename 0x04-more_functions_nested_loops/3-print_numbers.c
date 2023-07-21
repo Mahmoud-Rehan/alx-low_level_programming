6537f685f87e1a285fe793338222fec3b019db87
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-07-21 09:10:28 -0700

    C language

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers.
+ * Return: 0 Always
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+	return (0);
+}
