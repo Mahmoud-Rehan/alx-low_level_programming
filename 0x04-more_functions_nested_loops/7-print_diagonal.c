6b8e9f6f924e5bd882c555319e3b59f7f1eefda6
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-07-21 14:23:10 -0700

    C language

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,9 +9,13 @@ void print_diagonal(int n)
 {
 	int i, j;
 
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
 	for (i = 0; i < n; i++)
 	{
-		for (j = n - i; j < n; j++)
+		for (j = 0; j < i; j++)
 		{
 			_putchar(' ');
 		}
