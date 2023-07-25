46d5161969ed78c12c6147108df4bdcbb316cd34
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-07-25 05:16:04 -0700

    C language

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,13 +16,11 @@ void puts_half(char *str)
 		length++;
 	}
 
-	if (length % 2 != 0)
+	n = length / 2;
+
+	if (length % 2 == 1)
 	{
-		n = (length - 1) / 2;
-	}
-	else
-	{
-		n = length / 2;
+		n = (length + 1) / 2;
 	}
 
 	while (str[n] != '\0')
