0bd1a8253c4b779b6529f65f336e6226f33a8c4b
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-07-25 09:25:24 -0700

    C language

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+int _atoi(char *s)
+{
+	unsigned int counter = 0;
+	unsigned int size = 0;
+	unsigned int n = 0;
+	unsigned int r = 1;
+	unsigned int m = 1;
+	unsigned int i;
+
+	while (s[counter] != '\0')
+	{
+		if (size > 0 && (s[counter] < '0' || s[counter] > '9'))
+		{
+			break;
+		}
+
+		if (s[counter] == '-')
+		{
+			r = r * -1;
+		}
+
+		if (s[counter] >= '0' && s[counter] <= '9')
+		{
+			if (size > 0)
+			{
+				m = m * 10;
+			}
+			size++;
+		}
+		counter++;
+	}
+
+	for (i = counter - size; i < counter; i++)
+	{
+		n = n + (s[counter] - 48) * m;
+		m = m / 10;
+	}
+
+	return (n * r);
+}
