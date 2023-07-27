75a2fb8caaebdca2550f215acaa640ac72bd719f
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-07-27 13:08:55 -0700

    C language

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
-/**_strncpy - Copies a string.
- * @dest: The string
- * @src: The array
- * Return: Pointer to string
+/**
+ * _strncpy - copy a string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ * Return: dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
