87c26b414b2bad1aeca42d90f8e3b7f64fb54ea0
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-07-28 10:16:46 -0700

    C language

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+char *rot13(char *s)
+{
+	int i = 0;
+	int j;
+
+	char array1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char array2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == array1[j])
+			{
+				s[i] = array2[j];
+				break;
+			}
+		}
+		i++;
+	}
+	return (s);
+}
