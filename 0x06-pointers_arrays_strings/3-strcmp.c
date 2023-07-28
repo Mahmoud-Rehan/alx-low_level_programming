612934f08babfb1f7f52111628b75c2bdf3137fc
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-07-27 17:32:13 -0700

    C language

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * _strcmp - Compares two strings.
+ * @s1: The first string
+ * @s2: The second string
+ * Return: An integer value
+ */
+
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
