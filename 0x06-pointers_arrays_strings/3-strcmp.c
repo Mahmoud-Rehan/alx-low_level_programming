e3e474d91c04143344b403d308a99cb83f6af445
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-07-27 17:29:25 -0700

    C language

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int j = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+
+	if (i > j)
+	{
+		return (1);
+	}
+	else if (i < j)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (0);
+	}
+}
