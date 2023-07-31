860f24e19da19ed04c25061da728c80fd006cc30
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-07-31 06:25:31 -0700

    C language

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * _memset - Fills memory with a constant byte.
+ * @s: The buffer.
+ * @b: The constant byte.
+ * @n: The size of memory area pointed by (s).
+ * Return: The buffer s.
+ */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
