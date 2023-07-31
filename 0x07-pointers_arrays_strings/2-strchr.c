8449dfd94daf265c6d7929468bd26626a30e72f2
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-07-31 14:42:07 -0700

    C language

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *f;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			*f = s[i];
+			break;
+		}
+	}
+
+	if (f[0] != c)
+	{
+		return (0);
+	}
+
+	return (s);
+}
