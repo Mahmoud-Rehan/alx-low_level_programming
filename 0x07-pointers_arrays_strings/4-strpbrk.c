7d670aa96bb54e737715f01875cfcd9e885c54b0
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-07-31 16:18:11 -0700

    C language

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strbrk - Searches a string for any of a set of bytes.
+ * @s: The string.
+ * @accept: The string of chars.
+ * Return: The string s or 0.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+		}
+	}
+
+	return (0);
+}
