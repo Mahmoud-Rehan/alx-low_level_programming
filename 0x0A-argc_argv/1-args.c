24a730b615ba1ae46bb11a3d7236e23b4a15dfeb
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-08-04 16:45:04 -0700

    C language

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it.
+ * @argc: The number of arguments.
+ * @argv: The array of strings.
+ * Return: 0 Always.
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)*argv;
+	printf("%d\n", argc);
+	return (0);
+}
