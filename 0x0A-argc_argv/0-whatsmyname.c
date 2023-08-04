7ea5f52a82d1f07000368816e065442763c68e4d
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-08-04 16:34:49 -0700

    C language

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,13 @@
 #include "main.h"
 #include <stdio.h>
+
+/**
+ * main - print the program name.
+ * @argc: The number of arguments.
+ * @argv: The array of strings.
+ * Return: 0 Always.
+ */
+
 int main(int argc, char *argv[])
 {
 	(void)argc;
