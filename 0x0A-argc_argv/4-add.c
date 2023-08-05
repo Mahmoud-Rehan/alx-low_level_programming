1b8117d27521b0f7736046995c3f084f9eedc08a
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-08-04 17:50:56 -0700

    C language

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,58 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * check_num - check if there are digit.
+ * @str: array.
+ * Return: 0 or 1.
+ */
+
+int check_num(char *str)
+{
+	int count = 0;
+
+	while (count < strlen(str))
+	{
+		if (!isdigit(str[count]))
+		{
+			return (0);
+		}
+		count++;
+	}
+	return (1);
+}
+
+/**
+ * main - adds positive numbers..
+ * @argc: Count arguments.
+ * @argv: Arguments.
+ * Return: 0 Always.
+ */
+
+int main(int argc, char *argv[])
+{
+	int counter;
+	int n;
+	int sum = 0;
+
+	counter = 1;
+	while (counter < argc)
+	{
+		if (check_num(argv[counter]))
+		{
+			n = atoi(argv[counter]);
+			sum = sum + n;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		counter++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
