e6f89b1cd77377918e6dd8331829b689dfab25e7
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-08-07 15:48:10 -0700

    C language

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - Copy the string given as parameter.
+ * @str: A string.
+ * Return: Copy of the string.
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0;
+	char *ptr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	ptr = malloc(i * sizeof(char));
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		ptr[i] = str[i];
+	}
+	return (ptr);
+}
