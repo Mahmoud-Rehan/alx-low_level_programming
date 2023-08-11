ed0ebd5cb8a2d1e431bce885e0453526dcdd4495
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-08-10 19:45:15 -0700

    C language

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -43,15 +43,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	while (n >= l2 && i < (l1 + l2))
 	{
-		ptr[i] = s2[j];
-		i++;
-		j++;
+		ptr[i++] = s2[j++];
 	}
 	while (n < l2 && i < (l1 + n))
 	{
-		ptr[i] = s2[j];
-		i++;
-		j++;
+		ptr[i++] = s2[j++];
 	}
 	ptr[i] = '\0';
 	return (ptr);
