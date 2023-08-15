b23257170474de0f0d40ad5d342b087d93d71224
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-08-14 18:22:10 -0700

    C language

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,74 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * _cpy - copy two string.
+ * @dest: The copied string.
+ * @src: The string.
+ * @length: The length.
+ * Return: The copied string.
+ */
+
+char *_cpy(char *dest, char *src, int length)
+{
+	int i;
+
+	for (i = 0; i < length; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * new_dog - Creates a new dog.
+ * @name: The dog name.
+ * @age: The dog age.
+ * @owner: The owner.
+ * Return: The new dog.
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	int i = 0, j = 0;
+
+	while (name[i] != '\0')
+	{
+		i++;
+	}
+	while (owner[j] != '\0')
+	{
+		j++;
+	}
+	dog = malloc(sizeof(dog_t));
+
+	if (dog == NULL)
+	{
+		return (NULL);
+	}
+
+	dog->name = malloc(sizeof(i + 1));
+
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+
+	dog->owner = malloc(sizeof(j + 1));
+
+	if (dog->owner == NULL)
+	{
+		free(dog);
+		free(dog->owner);
+		return (NULL);
+	}
+
+	_cpy(dog->name, name, i);
+	_cpy(dog->owner, owner, j);
+	dog->age = age;
+	return (dog);
+}
