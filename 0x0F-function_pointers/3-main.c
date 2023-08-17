78b0902ce88f036a58a1da860c20d42b807be315
Mahmoud Rehan <mahmoudrehna38@gmail.com>
2023-08-16 18:18:17 -0700

    C language

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include "3-calc.h"
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Check the code.
+ * @argc: Arguments count.
+ * @argv: Arguments array.
+ * Return: 0 Always.
+ */
+
+int main(int argc, char *argv[])
+{
+	int n1, n2;
+	char *s;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[3]);
+	s = argv[2];
+
+	if (get_op_func(s) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*s == '/' && n2 == 0) || (*s == '%' && n2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(s)(n1, n2));
+	return (0);
+}
