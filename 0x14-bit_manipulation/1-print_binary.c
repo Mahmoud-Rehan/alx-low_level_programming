e857908b64d2958194c832be87c9d852200ff7ab
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2023-08-31 16:35:31 -0700

    C language

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_binary - Convert decimal to binary;
+ * @n: The number;
+ */
+
+void print_binary(unsigned long int n)
+{
+	int i = 50;
+	int j = 0;
+	unsigned int b;
+
+	while (i >= 0)
+	{
+		b = n >> i;
+		if (b & 1)
+		{
+			_putchar('1');
+			j++;
+		}
+		else if (j)
+			_putchar('0');
+		i--;
+	}
+	if (!j)
+		_putchar('0');
+}
