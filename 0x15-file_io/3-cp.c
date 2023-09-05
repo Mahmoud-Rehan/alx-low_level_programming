6a7184bc57bee891f4986bc2d2706e6d6558277d
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2023-09-05 10:19:43 -0700

    C language

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 	int file_from, file_to, r;
 	char *buffer;
 
-	if (args != 3)
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
