e437ec08903fc7618a94bd046f2c0f49e49113e1
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2023-09-04 17:03:02 -0700

    C language

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buffer;
+	ssize_t n;
+	ssize_t bytes;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	buffer = malloc(sizeof(char) * letters);
+	n = read(fd, buffer, letters);
+	bytes = write(STDOUT_FILENO, buffer, n);
+
+	return (bytes);
+}
