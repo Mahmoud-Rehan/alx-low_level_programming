2879521a1a68dc98203b8663b0f20c3120a5a572
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2023-09-04 20:06:37 -0700

    C language

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 	r = read(file_from, buffer, 1024);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-	while (r > 0)
+	do
 	{
 		if (file_from == -1 || r == -1)
 		{
@@ -42,7 +42,7 @@ int main(int argc, char *argv[])
 		}
 		r = read(file_from, buffer, 1024);
 		file_to = open(argv[2], O_WRONLY | O_APPEND);
-	}
+	} while (r > 0);
 
 	free(buffer);
 	close_file(file_from);
