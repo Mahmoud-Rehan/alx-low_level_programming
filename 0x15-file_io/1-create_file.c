8c5905f62b38575c837b44b1dc18e7b660fe1025
Mahmoud-Rehan <maohmoudrehan38@gmail.com>
2023-10-02 15:16:30 -0700

    C language

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, i;
+	int fd, i = 0;
 	ssize_t w;
 
 	if (filename == NULL)
@@ -19,8 +19,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (i = 0; text_content[i] != '\0'; i++)
-			;
+		while (text_content[i] != '\0')
+			i++;
 	}
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
