83abaa7fc458ed1a5f41734b4aa90a0604556928
Mahmoud-Rehan <maohmoudrehan38@gmail.com>
2023-11-13 09:18:55 -0800

    C language

---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks for alphabetic character.
+ *
+ * @c: The checked char.
+ *
+ * Return: 1 if c is alphabet, else 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c < 97 || c > 122) && (c < 65 || c > 90))
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
