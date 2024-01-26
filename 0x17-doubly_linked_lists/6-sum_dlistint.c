e308bf45357088e750c814b4a70272ee9ee80bf4
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-01-26 13:21:43 -0800

    C Language

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * sum_dlistint - Sums all the integers of the list.
+ * @head: Doubly linked list.
+ * Return: The sum of integers.
+ */
+
 int sum_dlistint(dlistint_t *head)
 {
 	int n = 0;
