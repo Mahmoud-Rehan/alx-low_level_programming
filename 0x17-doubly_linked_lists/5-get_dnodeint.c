92f0fc4ff011578efe4c41785dd3005e9c8c9a98
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-01-26 13:07:31 -0800

    C Language

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - Gets nth element of a list.
+ * @head: Doubly linked list.
+ * @index: index of element.
+ * Return: Element of a linked list.
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *tmp;
+	unsigned int n;
+
+	n = 0;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	tmp = head;
+
+	while (tmp != NULL)
+	{
+		if (n == index)
+		{
+			break;
+		}
+		n++;
+		tmp = tmp->next;
+	}
+       	return (tmp);
+}
