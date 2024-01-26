2d8e9f60e1215d1e2b6916acfc87bc5d75f162c9
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-01-26 11:47:28 -0800

    C language

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -3,6 +3,12 @@
 #include "lists.h"
 
 
+/*
+ * dlistint_len - Count the number of elements.
+ * @h: The doubly linked list.
+ * Return: The number of elements.
+ */
+
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t n = 0;
