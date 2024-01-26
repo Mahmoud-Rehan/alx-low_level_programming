32d47723009a853ca9c1a1de1b6c5e104e057ca9
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-01-26 11:45:00 -0800

    C language

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
