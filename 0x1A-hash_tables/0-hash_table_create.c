a4a1cce4dd13daecd5b39544c84d528d0cfe5fbc
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-02-16 13:42:45 -0800

    C Language

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,11 +1,12 @@
 #include "hash_tables.h"
 
-/*
- * hash_table_create - Creates a hash table.
+/**
+ * hash_table_create - Creates a new hash table.
  * @size: The size of the hash table.
  *
- * Return: Pointer to the created hash table.
+ * Return: The new hash table.
  */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
