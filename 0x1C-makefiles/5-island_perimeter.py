f3048c338c1c51a81e603f3cd8fb682f5882a098
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-04-09 04:24:20 -0700

    Python

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -10,28 +10,26 @@ def island_perimeter(grid):
         sublist = grid[i]
 
         for n in range(0, len(sublist) - 1):
+            
             if sublist[n] == 1:
-                if n != 0:
+                
+                if n > 0:
+                    
                     if sublist[n - 1] == 0:
                         perimeter += 1
-                if n != len(sublist) - 1:
+                
+                if n < len(sublist) - 1:
+                    
                     if sublist[n + 1] == 0:
                         perimeter += 1
 
-                if i == 0:
-                    downlist = grid[i + 1]
-                    if downlist[n] == 0:
-                        perimeter += 1
-                if i == len(grid) - 1:
-                    uplist = grid[i - 1]
-                    if uplist[n] == 0:
-                        perimeter += 1
-                else:
-                    downlist = grid[i + 1]
-                    uplist = grid[i - 1]
+                downlist = grid[i + 1]
+                uplist = grid[i - 1]
 
-                    if downlist[n] == 0:
-                        perimeter += 1
-                    if uplist[n] == 0:
-                        perimeter += 1
+                if downlist[n] == 0:
+                    perimeter += 1
+                
+                if uplist[n] == 0:
+                    perimeter += 1
+    
     return (perimeter)
